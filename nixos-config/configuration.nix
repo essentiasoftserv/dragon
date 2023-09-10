4ffccc27aee16d26036fb008aa7bc2a0b4fa91dd
vansh aggarwal <vanshaggarwal@vanshs-MacBook-Pro.local>
2023-09-10 13:46:14 +0530

    openLDAP and openVPN services added to the configuration file

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{config, pkgs, ... }:
 
 
 {
@@ -74,6 +74,65 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+
+  # openldap config
+  services.openldap = {
+    enable = true;
+
+    /* enable plain connections only */
+    urlList = [ "ldap:///" ];
+    rootpw = "password";  # change your root password here 
+
+    settings = {
+      attrs = {
+        olcLogLevel = "conns config";
+      };
+
+      children = {
+        "cn=schema".includes = [
+          "${pkgs.openldap}/etc/schema/core.ldif"
+          "${pkgs.openldap}/etc/schema/cosine.ldif"
+          "${pkgs.openldap}/etc/schema/inetorgperson.ldif"
+        ];
+
+        "olcDatabase={1}mdb".attrs = {
+          objectClass = [ "olcDatabaseConfig" "olcMdbConfig" ];
+
+          olcDatabase = "{1}mdb";
+          olcDbDirectory = "/var/lib/openldap/data";
+
+          olcSuffix = "dc=example,dc=com";
+
+          /* your admin account, do not use writeText on a production system */
+          olcRootDN = "cn=admin,dc=example,dc=com";
+          olcRootPW.path = pkgs.writeText "olcRootPW" "pass";
+
+          olcAccess = [
+            /* custom access rules for userPassword attributes */
+            ''{0}to attrs=userPassword
+                by self write
+                by anonymous auth
+                by * none''
+
+            /* allow read on anything else */
+            ''{1}to *
+                by * read''
+          ];
+        };
+      };
+    };
+  };
+
+
+  # openvpn config
+
+  services.openvpn.servers = {
+    officeVPN  = { config = '' config /root/nixos/openvpn/officeVPN.conf ''; };
+    homeVPN    = { config = '' config /root/nixos/openvpn/homeVPN.conf ''; };
+    serverVPN  = { config = '' config /root/nixos/openvpn/serverVPN.conf ''; };
+  };
+
+
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vansha = {
